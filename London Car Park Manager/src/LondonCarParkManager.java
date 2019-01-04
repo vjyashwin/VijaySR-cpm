16babb36282eb10c4b73081a69c7d22756f1af22
Vijay <vjyashwin@gmail.com>
2019-01-04 15:33:17 +0530

    LondonParkManager class file has been update with comments and have implementd few methods for the return menu

--- London Car Park Manager/src/LondonCarParkManager.java
+++ London Car Park Manager/src/LondonCarParkManager.java
@@ -1,7 +1,13 @@
+/**
+ * @author vjyashwin
+ *
+ *This is the Main class which implements the park, unpark, availability of parking
+ */
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
+
 
 
 public class LondonCarParkManager {
@@ -9,12 +15,12 @@ public class LondonCarParkManager {
 	
 	 Scanner sc = new Scanner(System.in);
 
-	    static int parkingSlotCount = 10;
+	    static int parkingSlotCount = 10; //total parking slots are 10
 	    static int carCounter;
-	    static int ticketNumber = 5000;
+	    static int ticketNumber = 5000; //ticket number always starts with 5000
 	    static EmptyParkingSlot emptySlot = new EmptyParkingSlot("Empty");
-	    //static List<Vehicle> slots = new ArrayList<Vehicle>();
-	    static List slots = new ArrayList();
+	    static List<Vehicle> slots = new ArrayList<Vehicle>();
+	  
 	    
 	
 
@@ -30,6 +36,11 @@ public class LondonCarParkManager {
 	        carParkManager.displayMainMenu();
 		}
 	
+	/**
+	 * 
+	 * Method to display the menu for the car parking 
+	 * 
+	 */
 	public void displayMainMenu(){
         try {
             sc = new Scanner(System.in);
@@ -54,6 +65,7 @@ public class LondonCarParkManager {
                 	carParkManager.parkCar();
                     break;
                 case 2:
+                	carParkManager.unparkCar();
                     break;
                 case 3:
                    break;
@@ -75,6 +87,13 @@ public class LondonCarParkManager {
 
     }
 	
+	/**
+	 * This is the method which implements the parking of the car based on the available slots
+	 * The parkings are allowed till the freeslots are available and after that the parking are restricted
+	 * A parked car is provided with a ticket no starting from 5000
+	 * 
+	 */
+	
 	
 	
 	public void parkCar(){
@@ -93,6 +112,7 @@ public class LondonCarParkManager {
                 ticketNumber++;
                 System.out.println("Your Car's registration ID is: "+car.getIdPlate());
                 System.out.println("\n your Parking Tocket no is: "+ticketNumber);
+                returnToMenu();
               
                   } else {
                 return;
@@ -104,6 +124,11 @@ public class LondonCarParkManager {
         }
 
     }
+	
+	/**
+	 * This method will find out the free slots for the parking and if the slots are zero 
+	 * it will restrict further parking
+	 */
 	
 	public void trackFreeSlots(){
         if(parkingSlotCount >0){
@@ -118,7 +143,90 @@ public class LondonCarParkManager {
         }
     }
 	
+	/**
+	 * 
+	 * This method is used to unpark the car. unpark the car and increment the freeslots count
+	 */
 	
+	public void unparkCar(){
+
+        System.out.println();
+        System.out.println("                  Delete a vehicle                   \n ");
+        if (carCounter == 0) {
+            System.out.println("Cannot Perform this action as there are no cars parked");
+       }else {
+            System.out.println("List of cars in the parking\n");
+            for (Vehicle vehicle : slots) {
+                String ID = vehicle.getIdPlate();
+                if (ID != null)
+                    System.out.println("\t" + ID+ "Ticket no is: "+ticketNumber);
+            }
+            System.out.println("\n your car's resiration number? : ");
+
+            try {
+
+                String unparkCar = sc.next();
+                parkingSlotCount++;
+                carCounter--;
+                               slots.remove(getIndexByProperty(unparkCar));
+
+            } catch (Exception e) {
+               System.out.print(e);
+            }
+        }
+        returnToMenu();
+      }
+	
+	/**
+	 * This method will get the registration id for the car and then validates if the car bearing
+	 * the ID is being parked in the parking lot.
+	 * @param yourString
+	 * @return
+	 */
+	
+	 private int getIndexByProperty(String yourString) {
+	        for (int i = 0; i < slots.size(); i++) {
+	            if (slots.get(i).getIdPlate() !=null && slots.get(i).getIdPlate().equalsIgnoreCase(yourString)) {
+	              return i;
+	            }
+	        }
+	        System.out.println("\nSorry! There is no car parked with the specified ID plate.\n");
+	        return -1;
+	    }
+	
+	 /**
+	  * This method get the input from the user whether to continue with the main menu or to exit the menu.
+	  * 
+	  */
+	 public void returnToMenu(){
+	        try {
+
+	            System.out.println("\n\tDo you want to continue with main menu?\n");
+	            System.out.println(" Please select your option \n 1. Yes \n 2. No");
+
+	            switch (sc.nextInt() ){
+	                //will go to the main menu
+	                case 1:
+	                    displayMainMenu();
+	                    break;
+	                //will terminate the program
+	                case 2:
+	                    System.exit(0);
+	                    break;
+	                //to track invalid options
+	                default:
+	                    System.out.println("\nInvalid option!");
+	                    displayMainMenu();
+	                    break;
+	            }
+
+	        }catch (Exception ex3){
+	            System.out.println("\nInvalid option!");
+	            displayMainMenu(); //return
+	        }
+
+
+	    }
 	
 	
 
