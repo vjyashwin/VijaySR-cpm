328ae50ee88e30f70a2df7a89e66af1f09f46c5d
Vijay <vjyashwin@gmail.com>
2019-01-04 13:21:14 +0530

    getters and setters for vehicle registration number

---
+++ London Car Park Manager/src/Car.java
@@ -0,0 +1,15 @@
+
+/**
+ * @author Vijay Srinivasa Raghavan
+ *
+ */
+public class Car extends Vehicle {
+       //getters and setters for Vehicle registration no
+    
+    public String getIdPlate() {return idPlate;}
+
+    public void setIdPlate(String idPlate) {
+        this.idPlate = idPlate;
+    }
+   }
+
