d02299c08f15909e1cb4405e5577fd82071ae895
Vijay <vjyashwin@gmail.com>
2019-01-04 13:17:55 +0530

    abstract class to get the vehicle ID Committed on 4th Jan 2019

---
+++ London Car Park Manager/src/Vehicle.java
@@ -0,0 +1,11 @@
+/**
+ * @author Vijay Srinivasa Raghavan
+ *
+ */
+public abstract class Vehicle{
+    
+    public String idPlate;
+    public abstract String getIdPlate();
+    public abstract void setIdPlate(String idPlate);
+
+}
