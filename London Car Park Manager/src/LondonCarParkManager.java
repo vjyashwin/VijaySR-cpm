b6cc012d4d8fbe046ad79ee86246bc0686841337
Vijay <vjyashwin@gmail.com>
2019-01-04 14:17:00 +0530

    ADD STATIC vARIABLES to the class

--- London Car Park Manager/src/LondonCarParkManager.java
+++ London Car Park Manager/src/LondonCarParkManager.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
+
 
 public class LondonCarParkManager {
+	
+	
+	 Scanner sc = new Scanner(System.in);
+
+	    static int parkingSlotCount = 10;
+	    static int carCounter;
+	    static int ticketNumber = 5000;
+	    static EmptyParkingSlot emptySlot = new EmptyParkingSlot("Empty");
+	    
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
+		
 	}
 
 }
